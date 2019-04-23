483c239bd20f68f729ca90b4c638ce8877393a92
Edgar Rios Navarro <edgar.gs@gmail.com>
2019-04-23 06:39:32 -0500

    sockets

--- server/server.js
+++ server/server.js
@@ -1,17 +1,52 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const path = require('path');
 
 const app = express();
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+let io = socketIO(server);
 
+io.on('connection', (client) => {
 
-app.listen(port, (err) => {
+    console.log('Cliente conectado');
+
+    client.emit('enviarMensaje', {
+        usuario: 'Admin',
+        mensaje: 'Bienvenido'
+    });
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    //Escuchar clientes
+    client.on('enviarMensaje', (data, callback)=>{
+        console.log(data);
+
+        client.broadcast.emit('enviarMensaje', data);
+
+        // if(mensaje.usuario) {
+        //     callback({
+        //         resp: 'OK'
+        //     });
+        // } else {
+        //     callback({
+        //         resp: 'KO'
+        //     });
+        // }
+    });
+
+});
+
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
